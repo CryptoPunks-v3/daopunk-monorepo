4da02492ce6afaafe5e4b885a4195df72d0a5708
natkins-dev <natkins.dev@gmail.com>
2023-09-10 12:03:14 +0200

    fix: placeholder punk

--- packages/punks-webapp/src/components/Banner/index.tsx
+++ packages/punks-webapp/src/components/Banner/index.tsx
@@ -8,21 +8,19 @@ import { Trans } from '@lingui/macro';
 const Banner = () => {
   return (
     <Section fullWidth={false} className={classes.bannerSection}>
-      <Col lg={6}>
+      <Col lg={7}>
         <div className={classes.wrapper}>
           <h1>
             <Trans>ONE DAO PUNK,</Trans>
             <br />
-            <Trans>EVERYDAY,</Trans>
+            <Trans>EVERY DAY,</Trans>
             <br />
             <Trans>FOREVER.</Trans>
           </h1>
         </div>
       </Col>
-      <Col lg={6}>
-        <div style={{ padding: '2rem' }}>
-          <Punk imgPath={punk} alt="DAO Punk" />
-        </div>
+      <Col lg={5}>
+        <Punk imgPath={punk} alt="DAO Punk" />
       </Col>
     </Section>
   );
