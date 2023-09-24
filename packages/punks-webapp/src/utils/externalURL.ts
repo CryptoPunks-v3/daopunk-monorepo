0462a41c4ab96381ee03c553d0e3094ca02db647
lukasz-glen <>
2023-09-24 11:44:12 +0200

    fix: social media links

--- packages/punks-webapp/src/utils/externalURL.ts
+++ packages/punks-webapp/src/utils/externalURL.ts
@@ -8,12 +8,12 @@ export enum ExternalURL {
 export const externalURL = (externalURL: ExternalURL) => {
   switch (externalURL) {
     case ExternalURL.discord:
-      return 'https://discord.gg/keNWmpdQD7';
+      return 'https://discord.gg/daopunks';
     case ExternalURL.twitter:
-      return 'http://twitter.com/punkersdao';
+      return 'https://twitter.com/daopunks_';
     case ExternalURL.notion:
       return 'https://www.notion.so/CryptoPunks-DAO-Treasury-Docs-db000b97f1384807a27463ac496b24f6';
     case ExternalURL.discourse:
-      return 'https://discourse.nouns.wtf/';
+      return 'https://discourse.daopunks.wtf/';
   }
 };
