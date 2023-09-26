45b83cd53e07e4641b7e00975bc5152cecce9668
lukasz-glen <>
2023-09-26 17:47:23 +0200

    fix: twitter link

--- packages/punks-webapp/src/utils/externalURL.ts
+++ packages/punks-webapp/src/utils/externalURL.ts
@@ -10,7 +10,7 @@ export const externalURL = (externalURL: ExternalURL) => {
     case ExternalURL.discord:
       return 'https://discord.gg/daopunks';
     case ExternalURL.twitter:
-      return 'https://twitter.com/daopunks_';
+      return 'https://twitter.com/daopunks';
     case ExternalURL.notion:
       return 'https://www.notion.so/CryptoPunks-DAO-Treasury-Docs-db000b97f1384807a27463ac496b24f6';
     case ExternalURL.discourse:
